1931172b83af9cb0f74037f3554bd89e2975369a
Dipam Sarkar <dipamsarkar180@gmail.com>
2022-08-30 00:34:44 +0530

    update

--- Assets/Scripts/Camera_Look.cs
+++ Assets/Scripts/Camera_Look.cs
@@ -12,7 +12,7 @@ public class Camera_Look : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame

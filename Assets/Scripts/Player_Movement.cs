6c124e9602b347261cd165eb7ff232bd09769e5a
Dipam Sarkar <dipamsarkar180@gmail.com>
2022-08-31 00:56:26 +0530

    update

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -6,19 +6,37 @@ public class Player_Movement : MonoBehaviour
 {
     public CharacterController controller;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
     public float speed = 12f;
     public float gravity = -19.8f;
+    public float jumpHeight = 3f;
+
+    private bool isGrounded;
 
     Vector3 velocity;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y<0f)
+        {
+            velocity.y = -2f;
+        }
         float moveX = Input.GetAxis("Horizontal");
         float moveZ = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * moveX + transform.forward * moveZ;
         controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         
         velocity.y += gravity * Time.deltaTime;

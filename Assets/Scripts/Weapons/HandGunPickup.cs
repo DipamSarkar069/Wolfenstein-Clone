a3ebf5796332f35b42e87a8c105c99c01055ea02
DipamSarkar069 <dipamsarkar180@gmail.com>
2022-10-14 00:51:23 +0530

    Update

--- Assets/Scripts/Weapons/HandGunPickup.cs
+++ Assets/Scripts/Weapons/HandGunPickup.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HandGunPickup : MonoBehaviour
 {
     public GameObject realHandGun;
     public GameObject fakeHandGun;
     public AudioSource GunPickup;
+    public GameObject pickUpDisplay;
 
     void OnTriggerEnter(Collider other)
     {
@@ -14,5 +16,8 @@ public class HandGunPickup : MonoBehaviour
         fakeHandGun.SetActive(false);
         GunPickup.Play();
         GetComponent<BoxCollider>().enabled = false;
+        pickUpDisplay.SetActive(false);
+        pickUpDisplay.GetComponent<Text>().text = "Picked up a Hand-Gun";
+        pickUpDisplay.SetActive(true);
     }
 }
--- Assets/Scripts/Weapons/HandgunAmmoPickup.cs
+++ Assets/Scripts/Weapons/HandgunAmmoPickup.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HandgunAmmoPickup : MonoBehaviour
 {
     
     public GameObject fakeAmmo;
     public AudioSource AmmoPickup;
+    public GameObject pickUpDisplay;
 
     void OnTriggerEnter(Collider other)
     {
@@ -14,6 +16,9 @@ public class HandgunAmmoPickup : MonoBehaviour
         fakeAmmo.SetActive(false);
         AmmoPickup.Play();
         GlobalAmmoCount.maxAmmo += 10;
-        
+        GetComponent<BoxCollider>().enabled = false;
+        pickUpDisplay.SetActive(false);
+        pickUpDisplay.GetComponent<Text>().text = "Picked up a Hand-Gun Ammo";
+        pickUpDisplay.SetActive(true);
     }
 }

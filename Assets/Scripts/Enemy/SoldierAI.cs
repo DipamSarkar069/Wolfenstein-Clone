25c6636c264d7657580332ee4dfac2b2ec94e621
DipamSarkar069 <dipamsarkar180@gmail.com>
2022-10-15 20:37:56 +0530

    update

--- Assets/Scripts/Enemy/SoldierAI.cs
+++ Assets/Scripts/Enemy/SoldierAI.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class SoldierAI : MonoBehaviour
 {
     public string hitTag;
+    public float fireRate = 0.5f;
+
     public bool lookingAtPlayer=false;
+    public bool isFiring = false;
+
     public GameObject soldier;
+
+    public AudioSource enemyFire;
 
     // Update is called once per frame
     void Update()
@@ -16,15 +22,26 @@ public class SoldierAI : MonoBehaviour
         {
             hitTag = hit.transform.tag;
         }
-        if(hitTag=="Player")
+        if(hitTag=="Player" && isFiring==false)
         {
-            soldier.GetComponent<Animator>().Play("demo_combat_shoot");
-            lookingAtPlayer = true;
+            StartCoroutine(EnemyFire());
         }
-        else
+        else if(hitTag!="Player")
         {
             soldier.GetComponent<Animator>().Play("demo_combat_idle");
             lookingAtPlayer = false;
         }
+    }
+
+    IEnumerator EnemyFire()
+    {
+        isFiring = true;
+        soldier.GetComponent<Animator>().Play("demo_combat_shoot",-1,0);
+        soldier.GetComponent<Animator>().Play("demo_combat_shoot");
+        enemyFire.Play();
+        lookingAtPlayer = true;
+        yield return new WaitForSeconds(fireRate);
+        isFiring = false;
+
     }
 }
